14cdb8a90370399dc0d05ab18f1c1b336b7a8831
chrisystems <51771601+chrisystems@users.noreply.github.com>
2025-05-06 19:29:24 -0500

    Update TestRunner.java

--- src/test/java/org/udea/parabank/TestRunner.java
+++ src/test/java/org/udea/parabank/TestRunner.java
@@ -5,16 +5,8 @@ import com.intuit.karate.junit5.Karate;
 class TestRunner {
 
     @Karate.Test
-    Karate test02_ParabankLogin() {
+    Karate test01_ParabankLogin() {
         return Karate.run("login")
-                .relativeTo(getClass())
-                .outputCucumberJson(true);
-    }
-
-    @Karate.Test
-    Karate test03_ParabankCustomers() {
-        return Karate.run()
-                .tags("@parabank_transfer")
                 .relativeTo(getClass())
                 .outputCucumberJson(true);
     }
